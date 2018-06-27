8a2e72983174b94b929a10a908a5cde7250b89d4
Andrew Parnell-Smith <andrewparnellsmith@gmail.com>
2018-06-27 05:04:26 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,9 +1,9 @@
-var playlist = {artist: 'song title'}
+var playlist = {artist: 'song title'};
 function updatePlaylist(playlist, artist, songTitle) {
-  playlist[artist] = 'songTitle'
-  return playlist
+  playlist[artist] = 'songTitle';
+  return playlist;
 }
-function removeFromPlaylist(playlist, artist) {
-  delete playlist.artist
-  return playlist
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+  return playlist;
 }
