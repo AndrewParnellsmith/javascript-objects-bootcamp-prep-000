3c36c01a36cb0f1d73fc214f3c66dc5fe6419a09
Andrew Parnell-Smith <andrewparnellsmith@gmail.com>
2018-06-27 04:51:42 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,5 @@
 var playlist = { Slowdive: 'Alison', 'My Bloody Valentine': 'Sometimes'}
  function updatePlaylist(playlist, atristName, songTitle) {
-  return Object.assign({}, playlist, { artristName: songTitle })
+  playlist[artistName] = songTitle
+  return playlist
  }
